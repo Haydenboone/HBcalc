bc5f80abc3bea9d8f0e439e435a141bd8581a113
cliftontrendsic <clifton@trendsic.com>
2025-01-27 12:00:22 -0600

    knockoutjs for select-options exercise:  finish out logic for remaining operations.

--- HBcalc/index.js
+++ HBcalc/index.js
@@ -3,17 +3,19 @@
     self.in1 = ko.observable(0);
     self.in2 = ko.observable(0);
     self.result = ko.observable();
+    self.listoperation = ko.observableArray(["+", "-", "*", "/"]);
+    self.selectedoperation = ko.observable();
 
     self.the1 = function () {
         var self = this;
-        self.result(+self.in1() * (+self.in2()));
+        if (self.selectedoperation() == '*') {
+            self.result((+self.in1()) * (+self.in2()));
+        } else if (self.selectedoperation() == '+') {
+            self.result((+self.in1()) + (+self.in2()));
+        } else {
+            self.result(-9999);
+        }
     }
-
-
-
-
-
-
 
 }
 ko.applyBindings(new indexViewModel(), document.querySelector("#main"));
