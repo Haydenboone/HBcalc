274d184809535c0ef1653c974e725cbb33165d7b
Hayden Boone <Haydenboone34@outlook.com>
2025-01-27 10:56:01 -0600

    help[

--- HBcalc/index.js
+++ HBcalc/index.js
@@ -1,5 +1,34 @@
 ﻿function indexViewModel() {
     var self = this;
+    self.in1 = ko.observable(0);
+    self.in2 = ko.observable(0);
+    self.result = ko.observable();
+
+    self.the1 = function () {
+        var self = this;
+        self.result(+self.in1() * (+self.in2()));
+    }
+
+
+
+
+
+
 
 }
 ko.applyBindings(new indexViewModel(), document.querySelector("#main"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
